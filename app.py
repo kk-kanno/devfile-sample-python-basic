9724989ccadee52f88bab7046c69daa41f2933d1
kk-kanno <kanno.kakeru@marble-corp.co.jp>
2025-01-15 10:34:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -62,63 +62,6 @@ def get():
              di["today"][name] = d
          return json.dumps(di)
 
-@app.route('/time')
-def time():
-     d = request.args.get("d")
-     list = d.split("_")
-     u = request.args.get("user")
-     di = {}
-     di["timeMachine"] = {}
-     di["timeMachine"]["recent"] = {}
-     di["timeMachine"]["past"] = {}
-     di["timeMachine"]["future"] = {}
-     with open(f"./data/user/{u}.json") as f:
-        str = json.load(f)
-        for i in range(len(str)):
-            if str[i] == f"user/{u}/メモ_{d}.md":
-                if i != 0:
-                    di["timeMachine"]["recent"]["prev"] = str[i-1]
-                if i != len(str)-1:
-                    di["timeMachine"]["recent"]["next"] = str[i+1]
-        for j in range(1,4):
-            for i in range(len(str)-1,-1,-1):
-                if int(str[i].split("/")[2].split("_")[1]) == int(list[0])-j and int(str[i].split("/")[2].split("_")[3].split(".")[0]) <= int(list[2]) and int(str[i].split("/")[2].split("_")[2]) == int(list[1]):
-                    di["timeMachine"]["past"][f"{j}"] = str[i]
-                    break
-                elif int(str[i].split("/")[2].split("_")[1]) == int(list[0])-j and int(str[i].split("/")[2].split("_")[2]) < int(list[1]):
-                    di["timeMachine"]["past"][f"{j}"] = str[i]
-                    break
-                elif int(str[i].split("/")[2].split("_")[1]) < int(list[0])-j:
-                    di["timeMachine"]["past"][f"{j}"] = str[i]
-                    break
-        for j in range(1,4):
-            for i in range(len(str)):
-                if int(str[i].split("/")[2].split("_")[1]) == int(list[0])+j and int(str[i].split("/")[2].split("_")[3].split(".")[0]) >= int(list[2]) and int(str[i].split("/")[2].split("_")[2]) == int(list[1]):
-                    di["timeMachine"]["future"][f"{j}"] = str[i]
-                    break
-                elif int(str[i].split("/")[2].split("_")[1]) == int(list[0])+j and int(str[i].split("/")[2].split("_")[2]) > int(list[1]):
-                    di["timeMachine"]["future"][f"{j}"] = str[i]
-                    break
-                elif int(str[i].split("/")[2].split("_")[1]) > int(list[0])+j:
-                    di["timeMachine"]["future"][f"{j}"] = str[i]
-                    break
-    return json.dumps(di)
-
-@app.route('/today')
-def today():
-    d = request.args.get("d")
-    list = d.split("_")
-    u = request.args.get("user")
-    di = {}
-    di["today"] = {}
-    with open(f"./data/all.json") as f:
-    str = json.load(f)
-    userdata = str[f"{d}"]
-    for d in userdata:
-        name = d.split("/")[1]
-        di["today"][name] = d
-    return json.dumps(di)
-
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
