b66a6e480946485a493a09daa7c0b9fee5daff80
kk-kanno <kanno.kakeru@marble-corp.co.jp>
2025-01-17 11:44:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! nice to meet you. good bye.
+    return "Hello World! nice to meet you. good bye."
 @app.route("/let")
 def play():
     return "Let's Go!! shall we dance?"
